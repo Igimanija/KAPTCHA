c8549816101788c299724b45e47d4625d8527f54
Marko Radulovic <marko.radulovic@mradulovic.com>
2023-04-15 17:53:29 +0200

    fetching user current info

--- server/middlewares.js
+++ server/middlewares.js
@@ -1,42 +1,47 @@
 const jwt = require('jsonwebtoken');
+
 const expiry_time = 4 * 60 * 60;
 
 function requireLogin(req, res, next) {
-    if (!req.session.username) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-}
-  
-function requireLogout(req, res, next) {
-    if (req.session.username) {
-      res.redirect('/lobby');
-    } else {
-      next();
-    }
-}
-
-function createJWT(username, trophies){
-  return jwt.sign({username, trophies}, "secure_secret", {
-      expiresIn: expiry_time
-  });
-}
-
-const authenticate = (req, res, next) => {
-  let token = req.cookies.token;
-  if(token){
-      jwt.verify(token, "secure_secret", (error, truetoken) => {
-          if(error){
-              console.log(error);
-              res.redirect("/login");
-          }else{
-              next();
-          }
-      });
-  }else{
-      res.redirect("/login");
+  if (!req.session.username) {
+    res.redirect('/login');
+  } else {
+    next();
   }
 }
 
-module.exports = { requireLogin, requireLogout, createJWT, authenticate};
+function requireLogout(req, res, next) {
+  if (req.session.username) {
+    res.redirect('/lobby');
+  } else {
+    next();
+  }
+}
+
+function createJWT(username) {
+  return jwt.sign({ username }, "secure_secret", {
+    expiresIn: expiry_time
+  });
+}
+
+
+
+const authenticate = (req, res, next) => {
+  let token = req.cookies.token;
+  if (token) {
+    jwt.verify(token, "secure_secret", (error, truetoken) => {
+      if (error) {
+        res.clearCookie("token");
+        req.session.destroy();
+        res.redirect("/login");
+      } else {
+        next();
+      }
+    });
+  } else {
+    req.session.destroy();
+    res.redirect("/login");
+  }
+}
+
+module.exports = { requireLogin, requireLogout, createJWT, authenticate };
--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const fs = require("fs");
 const router = express.Router();
 const db = require('./db');
-const { requireLogin, requireLogout, createJWT, authenticate} = require('./middlewares');
+const { requireLogin, requireLogout, createJWT, authenticate } = require('./middlewares');
 const server_room = [];
 const room_ids = new Map();
 
@@ -12,22 +12,37 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views', 'index.html'));
 });
 
-router.get('/lobby', authenticate, requireLogin,(req, res) => {
-    res.sendFile(path.join(__dirname, '../public/views', 'lobby.html'))});
+router.get('/lobby', authenticate, requireLogin, async (req, res) => {
+    const userInfo = await accountInfo2(req);
+    const filePath = path.join(__dirname, '../public/views', 'lobby.html');
+    fs.readFile(filePath, "utf-8", (err, fileContent) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error reading chatroom");
+            return;
+        }
+        const modified = fileContent
+            .replace("{{email}}", userInfo.email)
+            .replace("{{username}}", userInfo.username)
+            .replace("{{trophies}}", userInfo.trophies);
+        res.send(modified);
+    });
+
+});
 
 
 router.get('/login', requireLogout, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/views', 'login.html'));
 });
 
-router.get('/register',requireLogout, (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/views','register.html'));
+router.get('/register', requireLogout, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/views', 'register.html'));
 });
 
 router.post('/register', requireLogout, (req, res) => {
-    const { username,email, password } = req.body;
+    const { username, email, password } = req.body;
     db.query("Select username  from users where username = ?", req.body.username, function (error, result) {
-        if (result.length >0) {
+        if (result.length > 0) {
             res.redirect("/register");
             return;
         }
@@ -48,14 +63,16 @@ router.post("/login", requireLogout, (req, res) => {
         var testpass = crypto.createHash('sha256').update(req.body.password).digest('hex');
         if (result[0].password == testpass) {
             req.session.username = req.body.username;
-            let token = createJWT(result[0].username, result[0].trophies);
-            res.cookie('token', token, {httpOnly: true, maxAge: 4 * 60 * 60 * 1000});
+            let token = createJWT(result[0].username);
+            res.cookie('token', token, { httpOnly: true, maxAge: 4 * 60 * 60 * 1000 });
             res.redirect("/lobby");
         } else {
             res.redirect("/login");
         }
     });
 });
+
+
 
 router.post("/rooms/:username", requireLogin, (req, res) => {
     const { username } = req.params;
@@ -98,4 +115,35 @@ router.get("/chatroom/:id", requireLogin, (req, res) => {
     });
 });
 
+
+
+
+
+/*****
+ * 
+ * Lobby Info
+ */
+
+router.get("/accountInfo2", authenticate, requireLogin, async (req, res) => {
+    res.send(await accountInfo2(req))
+})
+
+function accountInfo2(req) {
+    return new Promise((resolve, reject) => {
+        db.query('SELECT * FROM users WHERE username = ?', [req.session.username], (err, result) => {
+            if (err) {
+                reject(err);
+            } else if (result.length === 0) {
+                reject(new Error('Account not found'));
+            } else {
+                const accountInfo = {
+                    username: result[0].username,
+                    email: result[0].email,
+                    trophies: result[0].trophies,
+                };
+                resolve(accountInfo);
+            }
+        });
+    });
+}
 module.exports = router;
