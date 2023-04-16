db034ed52dc87aabcc1d4121e8a62d69499d2826
Nikola Igic <nikolaigic@Nikolas-MacBook-Pro.local>
2023-04-16 15:51:51 +0200

    rest

--- public/scripts/lobby.js
+++ public/scripts/lobby.js
@@ -1,47 +1,50 @@
 // const username = session.get('username');
-const errorP = document.querySelector('#errorP')
+const errorP = document.querySelector("#errorP");
 
 async function createRoom() {
-    try {
-        const user = await fetch("/accountInfo2");
-        const jsonUser = await user.json();
-        const response = await fetch('/rooms/' + jsonUser.username, {
-            method: 'POST'
-        })
-        const json = await response.json();
-        if (json.message !== "Room already exists") {
-            location.href = "/room/" + jsonUser.username;
-            return;
-        }
-        errorP.innerHTML = json.message
-
-    } catch (e) {
-        console.log(e)
+  try {
+    const user = await fetch("/accountInfo2");
+    const jsonUser = await user.json();
+    const response = await fetch("/rooms/" + jsonUser.username, {
+      method: "POST",
+    });
+    const json = await response.json();
+    if (json.message !== "Room already exists") {
+      location.href = "/room/" + jsonUser.username;
+      return;
     }
+    errorP.innerHTML = json.message;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 async function getRooms() {
-    console.log("test123");
-    try {
-        const response = await fetch('/rooms');
-        const json = await response.json();
-        roomHtml(json)
-    } catch (error) {
-        return console.error(error);
-    }
+  try {
+    const response = await fetch("/rooms");
+    const data = await response.json();
+    const myMap = new Map(Object.entries(data));
+    roomHtml(myMap);
+  } catch (error) {
+    return console.error(error);
+  }
 }
 
 function roomHtml(rooms) {
-    const gameRoom = document.querySelector('.game_room');
-    for (const room of rooms) {
-        gameRoom.insertAdjacentHTML("beforeend", ` 
+  const gameRoom = document.querySelector(".game_room");
+  for (const [key, value] of rooms) {
+    gameRoom.insertAdjacentHTML(
+      "beforeend",
+      ` 
         <div class="room">
-            <div class="room-name">${room}</div>
-            <button>Play</button>
+            <div class="room-name">${key}</div>
+            <a href="/room/${key}">
+            <button>
+            Play
+            </button>
+            </a>
         </div>
-    `)
-    }
+    `
+    );
+  }
 }
-
-
-
